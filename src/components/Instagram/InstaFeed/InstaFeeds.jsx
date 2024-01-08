4fa21c7674a2caba0a6f36edf43ed1b72fd64dff
SHAFINSHAMUHAMMED <122475859+SHAFINSHAMUHAMMED@users.noreply.github.com>
2024-01-08 18:18:55 +0530

    08/01/2024

--- src/components/Instagram/InstaFeed/InstaFeeds.jsx
+++ src/components/Instagram/InstaFeed/InstaFeeds.jsx
@@ -164,10 +164,10 @@ function InstaFeeds({setHeaderSize,getComponentRef}) {
       <div className="flex justify-between items-center bg-white  border-gray-300 p-3 pt-3 pb-3">
         <h4
               type="text"
-              className=" input-hover w-[100%] text-[14px] font-medium  bg-transparent whitespace-nowrap"
+              className=" w-[100%] text-[14px] font-medium  bg-transparent whitespace-nowrap"
               style={{ outline: "none", lineHeight:'0px' }}
             > 
-            Download 
+            <span className="input-hover" contentEditable>  Download </span>
             </h4> 
 
         <svg width={15} data-v-45aa4ba6="" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 256 256" xml:space="preserve"><g data-v-45aa4ba6=""><g data-v-45aa4ba6=""><polygon data-v-45aa4ba6="" points="79.093,0 48.907,30.187 146.72,128 48.907,225.813 79.093,256 207.093,128 "></polygon></g></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g><g data-v-45aa4ba6=""></g></svg>
@@ -175,7 +175,7 @@ function InstaFeeds({setHeaderSize,getComponentRef}) {
       <hr />
       <div className="ps-3 pe-3 bg-white mt-3">
       <div className="flex justify-between ">
-              <div className="flex">
+              <div className="flex gap-1">
               <svg widths={15} data-v-45aa4ba6="" aria-label="Like" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path data-v-45aa4ba6="" d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>
               <svg widths={15} data-v-45aa4ba6="" aria-label="Comment" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path data-v-45aa4ba6="" d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
               <svg widths={15} data-v-45aa4ba6="" aria-label="Share Post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><line data-v-45aa4ba6="" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon data-v-45aa4ba6="" fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>
